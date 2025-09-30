43875451e14995d3c0dcb12a9b601d324d29da13
Lucas-AR <lucas-ar@hotmail.com>
2025-09-30 19:14:52 -0300

    Creating players and enemies Move players and enemies

--- code/entityFactory.py
+++ code/entityFactory.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from code.Const import WIN_WIDTH
+import random
+
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.background import Background
+from code.enemy import Enemy
+from code.player import Player
 
 
 class EntityFactory:
@@ -15,3 +19,11 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', position=(0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', position=(WIN_WIDTH, 0)))
                 return list_bg
+            case 'Player1':
+                return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
+            case 'Player2':
+                return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
+            case 'Enemy1':
+                return Enemy('Enemy1', (WIN_WIDTH + 15, random.randint(20, WIN_HEIGHT)))
+            case 'Enemy2':
+                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(20, WIN_HEIGHT)))
